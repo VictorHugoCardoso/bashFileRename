cdcb5e13f9e30d13cdf0581941989d096058bfa1
note <victor.mendes056@gmail.com>
2019-09-05 15:10:40 -0300

    GOHORDSE

---
+++ SouthPark/script.sh
@@ -0,0 +1,38 @@
+SEASON="11";
+cd South\ Park\ ${SEASON}\ temporada/
+for f in *;do
+    EPISODE=${f:3:2};
+    echo "$f";
+    echo "South.Park.S${SEASON}E${EPISODE}.DVDRip.${f##*.}";
+    echo "";
+done
+cd ".."
+
+SEASON="14";
+cd South\ Park\ ${SEASON}\ temporada/
+for f in *;do
+    EPISODE=${f:3:2};
+    echo "$f";
+    echo "South.Park.S${SEASON}E${EPISODE}.DVDRip.${f##*.}";
+    echo "";
+done
+cd ".."
+
+SEASON="15";
+cd South\ Park\ ${SEASON}\ temporada/
+for f in *;do
+    EPISODE=${f:3:2};
+    echo "$f";
+    echo "South.Park.S${SEASON}E${EPISODE}.DVDRip.${f##*.}";
+    echo "";
+done
+cd ".."
+
+SEASON="16";
+cd South\ Park\ ${SEASON}\ temporada/
+for f in *;do
+    EPISODE=${f:3:2};
+    echo "$f";
+    echo "South.Park.S${SEASON}E${EPISODE}.DVDRip.${f##*.}";
+    echo "";
+done
